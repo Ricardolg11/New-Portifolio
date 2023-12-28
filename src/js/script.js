d0c7d0a2b4164f49612b6107b6b345505511dab5
ricardo moises <solop162@gmail.com>
2023-12-28 19:35:12 -0300

    atualizando

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,7 @@
-const token="​nkXqdVdcTQAcWulaSu8G1CLE​"
 const btnBemVindo=document.querySelector('#sobre-mim');
 const apresentation=document.querySelector('#apresentation')
-const option={
-    headers:{
-        Authorization: `Bearer <${token}>`
-    },
-    method:"get"
-}
 
 btnBemVindo.addEventListener('click',(event)=>{
     event.preventDefault()
     apresentation.style.display="block"
 })
-
-
